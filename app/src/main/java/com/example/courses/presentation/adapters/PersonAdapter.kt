8b36f77d724163150c5631c6c0c076295c128a74
Ivgen <ivgenij.v@gmail.com>
2021-04-23 19:12:26 +0300

    listAdapter and diffUtilsExample

--- app/src/main/java/com/example/courses/presentation/adapters/PersonAdapter.kt
+++ app/src/main/java/com/example/courses/presentation/adapters/PersonAdapter.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.courses.R
 import com.example.courses.entity.Person
 import com.example.courses.presentation.adapters.ItemClickListener
+import com.example.courses.presentation.adapters.PersonsCompareCallback
 
 class PersonAdapter internal constructor(
-    private var data: List<Person>
-) : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
+
+) : ListAdapter<Person,PersonAdapter.ViewHolder>(PersonsCompareCallback()) {
     private var listener: ItemClickListener? = null
+
+
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
@@ -24,18 +28,21 @@ class PersonAdapter internal constructor(
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
+/*
         val item = data[position]
-        viewHolder.text.text = item.toString()
+*/
+       /* viewHolder.text.text = item.toString()
         viewHolder.itemView.setOnClickListener {
             listener?.onClick(item)
-        }
+        }*/
     }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = 0 /*data.size*/
 
     fun setData(data: List<Person>) {
-        this.data = data
-        notifyDataSetChanged()
+
+        submitList(data)
+
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
